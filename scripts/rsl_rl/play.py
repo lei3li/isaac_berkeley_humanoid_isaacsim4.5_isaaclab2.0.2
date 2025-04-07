bf96f8a4f03afcba7ba520ae566a88b8f827cd0d
Lei Li <lei3.li@tum.de>
2025-04-07 17:51:29 +0200

    now working with isaacsim v4.5 with isaaclab v2.0.2

--- scripts/rsl_rl/play.py
+++ scripts/rsl_rl/play.py
@@ -9,7 +9,7 @@
 
 import argparse
 
-from omni.isaac.lab.app import AppLauncher
+from isaaclab.app import AppLauncher
 
 # local imports
 import cli_args  # isort: skip
@@ -49,9 +49,9 @@ from rsl_rl.runners import OnPolicyRunner
 # Import extensions to set up environment tasks
 import berkeley_humanoid.tasks  # noqa: F401
 
-from omni.isaac.lab.utils.dict import print_dict
-from omni.isaac.lab_tasks.utils import get_checkpoint_path, parse_env_cfg
-from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper, export_policy_as_onnx
+from isaaclab.utils.dict import print_dict
+from isaaclab_tasks.utils import get_checkpoint_path, parse_env_cfg
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper, export_policy_as_onnx
 
 
 def main():
